4110f9a25264e24f9d2478331012b3c16f7f6002
Kenny Nathalia <99057679@mydavinci.nl>
2021-06-01 09:57:44 +0200

    small changes

--- start.js
+++ start.js
@@ -1,5 +1,13 @@
+console.log(parties.name);
+
 const start = document.querySelector('.start-button');
+const statements = document.getElementById('statements');
+const eens = document.getElementById("btnEens");
+const oneens = document.getElementById("btnOneens");
+const volgende = document.getElementById("btnVolgende");
+ 
 
 start.onclick = function(){
 	window.location.assign('stemwijzer.html');
 }
+
