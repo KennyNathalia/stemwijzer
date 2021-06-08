126da6bf163ee9c2f9bf4294e4292afd8ee090fb
Kenny Nathalia <99057679@mydavinci.nl>
2021-06-08 11:24:53 +0200

    Statements and TItles

--- start.js
+++ start.js
@@ -1,13 +1,39 @@
-console.log(parties.name);
-
 const start = document.querySelector('.start-button');
-const statements = document.getElementById('statements');
+const statement = document.getElementById('statement');
+const title = document.getElementById('title');
+const buttons = document.querySelector('.buttons');
 const eens = document.getElementById("btnEens");
 const oneens = document.getElementById("btnOneens");
 const volgende = document.getElementById("btnVolgende");
+var statementCounter = 0;
+var titleCounter = 0;
  
+console.log(subjects);
+
 
 start.onclick = function(){
-	window.location.assign('stemwijzer.html');
+	start.style.display="none";
+    buttons.style.display="block";
+    displayStatement();
 }
 
+function displayStatement(){
+	title.innerHTML = subjects[statementCounter].title;
+	statement.innerHTML = subjects[statementCounter].statement;
+}	
+
+function nextStatement(){
+	if(titleCounter != 3){
+		statementCounter++;
+		titleCounter++;
+		displayStatement();
+	}
+}
+
+function previousStatement(){
+	if(statementCounter != 0){
+		statementCounter--;
+		titleCounter--;
+		displayStatement();
+	}
+}
